c58b6dbcff0240d38695799b5a0b1d331d6f82a8
Crowdbotics <team@crowdbotics.com>
2020-10-23 19:49:24 +0000

    Update models

--- backend/twobrains_21897/wsgi.py
+++ backend/twobrains_21897/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twobrains_21897.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twobrains_21897.settings")
 
 application = get_wsgi_application()
